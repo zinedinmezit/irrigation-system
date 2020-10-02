256dafb3937cacea15d68300e89653993cdd899e
breakfast-maker <zinedin.mezit@edu.fit.ba>
2020-10-02 21:40:18 +0200

    ESP8266 connection to WiFi

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char* ssid = "Tenda_29DC88";
+const char* password = "alifakovac1";
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(10);
+  Serial.println("\n");
+
+  WiFi.begin(ssid,password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\n");
+  Serial.println("Connected");
+  Serial.println(WiFi.localIP());
+
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
