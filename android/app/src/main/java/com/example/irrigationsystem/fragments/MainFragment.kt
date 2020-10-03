3836e7ab2b8bdb3934244c20031383df95e01f94
breakfast-maker <zinedin.mezit@edu.fit.ba>
2020-10-03 21:43:23 +0200

    Navigation to secondary fragment

--- android/app/src/main/java/com/example/irrigationsystem/fragments/MainFragment.kt
+++ android/app/src/main/java/com/example/irrigationsystem/fragments/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.irrigationsystem.R
 
 class MainFragment : Fragment() {
@@ -17,4 +19,12 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.button)
+        button?.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToSecondaryFragment()
+            this.findNavController().navigate(action)
+        }
+    }
 }
