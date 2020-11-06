95ec1d340d1deddad83c5df9c56d85d0e353c62c
zinedinmezit <zinedin.mezit@edu.fit.ba>
2020-11-06 23:30:25 +0100

    Support for the up button

--- android/app/src/main/java/com/example/irrigationsystem/MainActivity.kt
+++ android/app/src/main/java/com/example/irrigationsystem/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.irrigationsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp()
     }
 }
