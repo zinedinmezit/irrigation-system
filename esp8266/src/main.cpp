3a08010e7d8fe9ecbfd8117eeafc1c628e6fa331
zinedinmezit <zinedin.mezit@edu.fit.ba>
2021-01-17 15:54:13 +0100

    Refactoring, sending JSON to client instead of String

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -4,100 +4,82 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <DHT.h>
+#include <ArduinoJson.h>
 
 #define DHTTYPE DHT11
 
+//WiFi credentials
 const char *ssid = "Tenda_29DC88";
 const char *password = "alifakovac1";
 
+//Watering intervals
 const char *waterOption1 = "2000";
 const char *waterOption2 = "5000";
 const char *waterOption3 = "10000";
 
 int relayPin = 12;
-int roomTemperaturePin = 14;
+int dht11Pin = 14;
 
 unsigned long previousMillis = 0;
 const long interval = 2000;
-int hummidityPin = A0;
-int value = 0;
+int moisturePin = A0;
+int moistureAnalogValue = 0;
 
-DHT dht(roomTemperaturePin, DHTTYPE);
+DHT dht(dht11Pin, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+StaticJsonDocument<96> sensorValues;
 
 WebSocketsServer webSocket(81);
 
 void ConnectToWiFi(const char *ssid, const char *password);
-
-void WateringDelay(const long interval)
-{
-  unsigned long trackMillis = 0;
-  unsigned long currentMillis = millis();
-
-  while (trackMillis < currentMillis + interval)
-  {
-    trackMillis = millis();
-    yield();
-  }
-}
+void WateringDelay(const long interval);
+void sendSensorData(float hummidity, float temperature, String moisture);
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
 {
   switch (type)
   {
   case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected\n", num);
     break;
+
   case WStype_CONNECTED:
-  {
-    Serial.printf("[%u] Connected\n", num);
     break;
-  }
+
   case WStype_TEXT:
   {
-
     const char *payloadText = (const char *)payload;
     if (strcmp(payloadText, waterOption1) == 0)
     {
       digitalWrite(relayPin, LOW);
       WateringDelay(2000);
       digitalWrite(relayPin, HIGH);
-      Serial.println(payloadText);
     }
     else if (strcmp(payloadText, waterOption2) == 0)
     {
       digitalWrite(relayPin, LOW);
       WateringDelay(5000);
       digitalWrite(relayPin, HIGH);
-      Serial.println(payloadText);
     }
     else if (strcmp(payloadText, waterOption3) == 0)
     {
       digitalWrite(relayPin, LOW);
       WateringDelay(10000);
       digitalWrite(relayPin, HIGH);
-      Serial.println(payloadText);
     }
-    else
-    {
-      Serial.println(payloadText);
-    }
-
     break;
   }
+
   case WStype_ERROR:
-    Serial.println("Error happened");
     break;
   }
 }
 
 void setup()
 {
-  Serial.begin(9600);
 
-  delay(10);
-  Serial.println("\n");
   dht.begin();
+
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -105,8 +87,8 @@ void setup()
   lcd.setCursor(0, 1);
 
   pinMode(relayPin, OUTPUT);
-
   digitalWrite(relayPin, HIGH);
+
   ConnectToWiFi(ssid, password);
 
   webSocket.begin();
@@ -121,45 +103,67 @@ void loop()
   if (currentMillis - previousMillis >= interval)
   {
     previousMillis = currentMillis;
-    value = analogRead(hummidityPin);
-    String result = (String)(100 - ((value / 1024.00) * 100));
+    moistureAnalogValue = analogRead(moisturePin);
+    String moistureValueString = (String)(100 - ((moistureAnalogValue / 1024.00) * 100));
 
     float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
     float t = dht.readTemperature();
 
-    if (isnan(h) || isnan(t))
-    {
-      Serial.println(F("Failed to read from DHT sensor!"));
-    }
-    else
-    {
-      // Compute heat index in Celsius (isFahreheit = false)
-      String append = result + "|" + (String)h + "|" + String(t);
-      Serial.print(F("Humidity: "));
-      Serial.print(h);
-      Serial.print(F("%  Temperature: "));
-      Serial.print(t);
-      Serial.print(F("°C "));
-      webSocket.sendTXT(0, append);
-    }
+    sendSensorData(h, t, moistureValueString);
   }
 }
 
 void ConnectToWiFi(const char *ssid, const char *password)
 {
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print(".");
     delay(1000);
   }
 
   String localIP = WiFi.localIP().toString();
-  Serial.println("\n");
-  Serial.println("Connected");
   lcd.print(localIP);
+}
+
+void WateringDelay(const long interval)
+{
+  unsigned long trackMillis = 0;
+  unsigned long currentMillis = millis();
+
+  while (trackMillis < currentMillis + interval)
+  {
+    trackMillis = millis();
+    yield();
+  }
+}
+
+void sendSensorData(float hummidity, float temperature, String moisture)
+{
+
+  sensorValues.clear();
+
+  if (moisture.isEmpty())
+    sensorValues["error_moisture"] = "Can't read moisture";
+  else
+  {
+    sensorValues["moisture"] = moisture;
+    sensorValues["error_moisture"] = "";
+  }
+
+  if (isnan(hummidity) || isnan(temperature))
+  {
+    sensorValues["error_dht11"] = "Can't read dht11";
+  }
+  else
+  {
+    sensorValues["moisture"] = moisture;
+    sensorValues["dhtTemp"] = (String)temperature;
+    sensorValues["dhtHumm"] = (String)hummidity;
+    sensorValues["error_dht11"] = "";
+  }
+
+  String output;
+  serializeJson(sensorValues, output);
+  webSocket.sendTXT(0, output);
 }
